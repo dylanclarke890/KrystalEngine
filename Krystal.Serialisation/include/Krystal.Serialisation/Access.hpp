d1e967f22790bc5b8922ce011e5b19f53c877c3b
Dylan <dylanclarke890@gmail.com>
2025-10-05 01:47:39 -0600

    drop unnecessary requires and add versioned overloads

--- Krystal.Serialisation/include/Krystal.Serialisation/Access.hpp
+++ Krystal.Serialisation/include/Krystal.Serialisation/Access.hpp
@@ -4,31 +4,25 @@
 
 namespace Krys::Serialisation
 {
-  /// @brief Allows archives to use private Transfer/Save/Load methods of types by being a friend. There are
-  /// require expressions on each method template that ensures the template is only instantiated if the
-  /// requested method actually exists on the target type, allowing the dispatcher to fallback to other
-  /// methods as needed.
+  /// @brief Allows archives to use private Transfer/Save/Load methods of types by being a friend.
   class Access
   {
   public:
 #pragma region Non-Versioned Member
 
     template <typename Archive, typename T>
-    requires requires(Archive &archive, T &value) { value.Transfer(archive); }
     static auto MemberTransfer(Archive &archive, T &value) noexcept -> decltype(value.Transfer(archive))
     {
       return value.Transfer(archive);
     }
 
     template <typename Archive, typename T>
-    requires requires(Archive &archive, const T &value) { value.Save(archive); }
-    static auto MemberSave(Archive &archive, T const &value) noexcept -> decltype(value.Save(archive))
+    static auto MemberSave(Archive &archive, const T &value) noexcept -> decltype(value.Save(archive))
     {
       return value.Save(archive);
     }
 
     template <typename Archive, typename T>
-    requires requires(Archive &archive, T &value) { value.Load(archive); }
     static auto MemberLoad(Archive &archive, T &value) noexcept -> decltype(value.Load(archive))
     {
       return value.Load(archive);
@@ -39,21 +33,18 @@ namespace Krys::Serialisation
 #pragma region Non-Versioned Non-Member
 
     template <typename Archive, typename T>
-    requires requires(Archive &archive, T &value) { Transfer(archive, value); }
     static auto NonMemberTransfer(Archive &archive, T &value) noexcept -> decltype(Transfer(archive, value))
     {
       return Transfer(archive, value);
     }
 
     template <typename Archive, typename T>
-    requires requires(Archive &archive, const T &value) { Save(archive, value); }
-    static auto NonMemberSave(Archive &archive, T const &value) noexcept -> decltype(Save(archive, value))
+    static auto NonMemberSave(Archive &archive, const T &value) noexcept -> decltype(Save(archive, value))
     {
       return Save(archive, value);
     }
 
     template <typename Archive, typename T>
-    requires requires(Archive &archive, T &value) { Load(archive, value); }
     static auto NonMemberLoad(Archive &archive, T &value) noexcept -> decltype(Load(archive, value))
     {
       return Load(archive, value);
@@ -64,25 +55,20 @@ namespace Krys::Serialisation
 #pragma region Versioned Member
 
     template <typename Archive, typename T>
-    requires requires(Archive &archive, T &value, const Version version) { value.Transfer(archive, version); }
-    static auto MemberTransfer(Archive &archive, T &value, const Version version) noexcept
+    static auto MemberTransfer(Archive &archive, T &value, Version version) noexcept
       -> decltype(value.Transfer(archive, version))
     {
       return value.Transfer(archive, version);
     }
 
     template <typename Archive, typename T>
-    requires requires(Archive &archive, const T &value, const Version version) {
-      value.Save(archive, version);
-    }
-    static auto MemberSave(Archive &archive, T const &value, const Version version) noexcept
+    static auto MemberSave(Archive &archive, const T &value, const Version version) noexcept
       -> decltype(value.Save(archive, version))
     {
       return value.Save(archive, version);
     }
 
     template <typename Archive, typename T>
-    requires requires(Archive &archive, T &value, const Version version) { value.Load(archive, version); }
     static auto MemberLoad(Archive &archive, T &value, const Version version) noexcept
       -> decltype(value.Load(archive, version))
     {
@@ -94,9 +80,6 @@ namespace Krys::Serialisation
 #pragma region Versioned Non-Member
 
     template <typename Archive, typename T>
-    requires requires(Archive &archive, T &value, const Version version) {
-      Transfer(archive, value, version);
-    }
     static auto NonMemberTransfer(Archive &archive, T &value, const Version version) noexcept
       -> decltype(Transfer(archive, value, version))
     {
@@ -104,17 +87,13 @@ namespace Krys::Serialisation
     }
 
     template <typename Archive, typename T>
-    requires requires(Archive &archive, const T &value, const Version version) {
-      Save(archive, value, version);
-    }
-    static auto NonMemberSave(Archive &archive, T const &value, const Version version) noexcept
+    static auto NonMemberSave(Archive &archive, const T &value, const Version version) noexcept
       -> decltype(Save(archive, value, version))
     {
       return Save(archive, value, version);
     }
 
     template <typename Archive, typename T>
-    requires requires(Archive &archive, T &value, const Version version) { Load(archive, value, version); }
     static auto NonMemberLoad(Archive &archive, T &value, const Version version) noexcept
       -> decltype(Load(archive, value, version))
     {
