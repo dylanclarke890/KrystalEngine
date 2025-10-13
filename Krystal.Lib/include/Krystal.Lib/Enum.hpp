87e818696b9a85845b3057b045d6782bfbe87a44
Dylan <dylanclarke890@gmail.com>
2025-10-13 00:23:08 -0600

    enum helpers

---
+++ Krystal.Lib/include/Krystal.Lib/Enum.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "Krystal.Lib/Types.hpp"
+#include <bit>
+#include <utility>
+
+namespace Krys
+{
+  /// @brief Concept that checks if a type is an enumeration.
+  template <typename TEnum>
+  concept Enumeration = std::is_enum_v<TEnum>;
+
+  template <Enumeration TEnum>
+  constexpr int32 OrdinalCount();
+
+  template <typename TEnum>
+  concept HasOrdinality = (OrdinalCount<TEnum>() > 0);
+
+  /// @brief Count of bits needed to represent every ordinal.
+  template <HasOrdinality TEnum>
+  constexpr int32_t BitCount()
+  {
+    return std::bit_width(static_cast<std::underlying_type_t<TEnum>>(OrdinalCount<TEnum>() - 1));
+  }
+
+  /// @brief Convenience function to iterate through every value in a Krys enum as part of
+  /// a range-based for loop.
+  template <HasOrdinality TEnum>
+  auto Ordinals()
+  {
+    struct Iterator
+    {
+      TEnum e {};
+
+      TEnum operator*() const
+      {
+        return e;
+      }
+
+      Iterator &operator++()
+      {
+        e = static_cast<TEnum>(std::to_underlying(e) + 1);
+        return *this;
+      }
+
+      bool operator==(const Iterator &other) const = default;
+      bool operator!=(const Iterator &other) const = default;
+    };
+
+    struct Range
+    {
+      Iterator begin() const
+      {
+        return Iterator {};
+      }
+      Iterator end() const
+      {
+        return Iterator {static_cast<TEnum>(OrdinalCount<TEnum>())};
+      }
+    };
+
+    return Range {};
+  }
+}
