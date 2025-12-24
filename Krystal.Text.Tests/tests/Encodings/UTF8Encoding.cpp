2f6b7d964b0e45b87f8ee956e2ed8e0255b44b3e
Dylan <dylanclarke890@gmail.com>
2025-12-24 15:48:36 -0700

    add test case for UTF8Encoding - it's the native encoding of strings for Krystal so not much testing is needed

---
+++ Krystal.Text.Tests/tests/Encodings/UTF8Encoding.cpp
@@ -0,0 +1,17 @@
+﻿#include "Krystal.Text/Encodings/UTF8Encoding.hpp"
+#include <catch_all.hpp>
+
+namespace Krys::Tests
+{
+  using namespace Krys::Text;
+
+  TEST_CASE("UTF8Encoding(Valid)", "[Text][Encodings][ASCII]")
+  {
+    UTF8Encoding asciiEncoding;
+
+    utf8_string text = u8"Hello, World! 123";
+    List<byte> encoded = asciiEncoding.Encode(text);
+    utf8_string decoded = asciiEncoding.Decode(encoded);
+    REQUIRE(decoded == text);
+  }
+}
