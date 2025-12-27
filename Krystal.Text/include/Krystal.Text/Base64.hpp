02954910b18ae03eeb1ec98deaef2f5fdca2950c
Dylan <dylanclarke890@gmail.com>
2025-12-26 18:04:17 -0700

    wip base64

---
+++ Krystal.Text/include/Krystal.Text/Base64.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "Krystal.Lib/Span.hpp"
+#include "Krystal.Lib/Types.hpp"
+
+namespace Krys::Text
+{
+  enum class Base64EncodeFlags : uint8
+  {
+    URL = 1 << 0,
+    OmitPadding = 1 << 1,
+  };
+
+  enum class Base64DecodeFlags : uint8
+  {
+    URL = 1 << 0,
+    ValidatePadding = 1 << 1,
+    IgnoreWhitespace = 1 << 2,
+  };
+
+  struct Base64Specification
+  {
+    Span<const byte> Input;
+    Base64EncodeFlags Options;
+  };
+}
