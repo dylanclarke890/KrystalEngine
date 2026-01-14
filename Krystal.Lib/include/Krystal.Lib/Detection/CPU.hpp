56e910ec2c70f3c1b88e1c9376a998ca23e1eeb6
Dylan <dylanclarke890@gmail.com>
2026-01-14 06:29:14 -0700

    merge byte order into cpu.hpp

--- Krystal.Lib/include/Krystal.Lib/Detection/CPU.hpp
+++ Krystal.Lib/include/Krystal.Lib/Detection/CPU.hpp
@@ -197,3 +197,17 @@
 #if !KRYS_CPU(KNOWN)
   #define KRYS_CPU_UNKNOWN 1
 #endif
+
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+  #define KRYS_CPU_BIG_ENDIAN 1
+#elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+  #define KRYS_CPU_LITTLE_ENDIAN 1
+#elif __BYTE_ORDER__ == __ORDER_PDP_ENDIAN__
+  #define KRYS_CPU_MIDDLE_ENDIAN 1
+#else
+  #error "Unknown endian"
+#endif
+
+#if !KRYS_CPU(BIG_ENDIAN) && !KRYS_CPU(LITTLE_ENDIAN)
+  #error "Unsupported endian"
+#endif
