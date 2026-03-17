66fcfeacafe9dc13603bda95833bebc6ec094a5f
Dylan <dylanclarke890@gmail.com>
2026-03-17 09:32:21 -0600

    add constructor for html slot element

---
+++ Krystal.HTML/src/HTMLElement/HTMLSlotElement.cpp
@@ -0,0 +1,12 @@
+﻿#include "Krystal.HTML/HTMLElement/HTMLSlotElement.hpp"
+#include "Krystal.HTML/Abort/AbortSignal.hpp"
+#include "Krystal.HTML/Node/CustomElementRegistry.hpp"
+#include "Krystal.HTML/Node/ShadowRoot.hpp"
+
+namespace Krys::HTML
+{
+  HTMLSlotElement::HTMLSlotElement(Document &document) noexcept
+      : HTMLElement(document, NodeFlag::IsHTMLSlotElement)
+  {
+  }
+}
