0e88de7abbb3fbead2571c3240634a1365dd1de0
Dylan <dylanclarke890@gmail.com>
2026-04-19 10:37:24 -0600

    added boilerplate for NamedNodeMap

---
+++ Krystal.HTML/include/Krystal.HTML/Node/NamedNodeMap.hpp
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "Krystal.HTML/DOMString.hpp"
+#include "Krystal.HTML/Utils/ExceptionOr.hpp"
+#include "Krystal.Lib/Core/Attributes.hpp"
+#include "Krystal.Lib/Pointers/RawPtr.hpp"
+#include "Krystal.Lib/Pointers/RefPtr.hpp"
+#include "Krystal.Lib/Pointers/WeakPtr.hpp"
+#include "Krystal.Lib/Types/Numeric.hpp"
+
+namespace Krys::HTML
+{
+  class Attr;
+  class Element;
+
+  class NamedNodeMap
+  {
+  private:
+    WeakRef<Element> _associatedElement;
+
+  public:
+    explicit NamedNodeMap(Element &associatedElement) noexcept
+        : _associatedElement(CreateWeakRef(associatedElement))
+    {
+    }
+
+    KRYS_NODISCARD size_t Length() noexcept;
+
+    KRYS_NODISCARD RawPtr<Attr> Item(size_t index) noexcept;
+
+    KRYS_NODISCARD RawPtr<Attr> NamedItem(const DOMString &qualifiedName) noexcept;
+
+    KRYS_NODISCARD RawPtr<Attr> NamedItemNS(const DOMString &attrNamespace,
+                                            const DOMString &localName) noexcept;
+
+    RawPtr<Attr> SetNamedItem(Attr &attr) noexcept;
+
+    RawPtr<Attr> SetNamedItemNS(Attr &attr) noexcept;
+
+    ExceptionOr<Ref<Attr>> RemoveNamedItem(const DOMString &qualifiedName) noexcept;
+
+    ExceptionOr<Ref<Attr>> RemoveNamedItemNS(const DOMString &attrNamespace,
+                                             const DOMString &localName) noexcept;
+  };
+}
---
+++ Krystal.HTML/src/Node/NamedNodeMap.cpp
@@ -0,0 +1,51 @@
+﻿#include "Krystal.HTML/Node/NamedNodeMap.hpp"
+#include "Krystal.HTML/Abort/AbortSignal.hpp"
+#include "Krystal.HTML/Node/Attr.hpp"
+#include "Krystal.HTML/Node/CustomElementRegistry.hpp"
+#include "Krystal.HTML/Node/Element.hpp"
+#include "Krystal.HTML/Node/ShadowRoot.hpp"
+
+// TODO(IMPL): Implement this class
+namespace Krys::HTML
+{
+  size_t NamedNodeMap::Length() noexcept
+  {
+    return 0uz;
+  }
+
+  RawPtr<Attr> NamedNodeMap::Item(size_t index) noexcept
+  {
+    return nullptr;
+  }
+
+  RawPtr<Attr> NamedNodeMap::NamedItem(const DOMString &qualifiedName) noexcept
+  {
+    return nullptr;
+  }
+
+  RawPtr<Attr> NamedNodeMap::NamedItemNS(const DOMString &attrNamespace, const DOMString &localName) noexcept
+  {
+    return nullptr;
+  }
+
+  RawPtr<Attr> NamedNodeMap::SetNamedItem(Attr &attr) noexcept
+  {
+    return nullptr;
+  }
+
+  RawPtr<Attr> NamedNodeMap::SetNamedItemNS(Attr &attr) noexcept
+  {
+    return nullptr;
+  }
+
+  ExceptionOr<Ref<Attr>> NamedNodeMap::RemoveNamedItem(const DOMString &qualifiedName) noexcept
+  {
+    return Exception {ExceptionCode::NotSupportedError};
+  }
+
+  ExceptionOr<Ref<Attr>> NamedNodeMap::RemoveNamedItemNS(const DOMString &attrNamespace,
+                                                         const DOMString &localName) noexcept
+  {
+    return Exception {ExceptionCode::NotSupportedError};
+  }
+}
