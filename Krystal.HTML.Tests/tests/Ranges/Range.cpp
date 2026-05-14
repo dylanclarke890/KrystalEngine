07d9606253d2581d74bfb4496bc83fe25a66e808
Dylan <dylanclarke890@gmail.com>
2026-05-14 16:13:28 -0600

    unit tests for StaticRange

---
+++ Krystal.HTML.Tests/tests/Ranges/Range.cpp
@@ -0,0 +1,17 @@
+﻿#include "Krystal.HTML/Ranges/Range.hpp"
+#include "Krystal.HTML.Tests/TestContainerNode.hpp"
+#include "Krystal.HTML/Abort/AbortSignal.hpp"
+#include "Krystal.HTML/CustomElement/CustomElementRegistry.hpp"
+#include "Krystal.HTML/Node/Attr.hpp"
+#include "Krystal.HTML/Node/Document.hpp"
+#include "Krystal.HTML/Node/NodeList.hpp"
+#include <catch_all.hpp>
+
+namespace Krys::Tests
+{
+  using namespace Krys::HTML;
+
+  TEST_CASE("Range", "[HTML][Range]")
+  {
+  }
+}
