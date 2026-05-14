67e5a749d6e1a62ddb3b086587308f3a0b589933
Dylan <dylanclarke890@gmail.com>
2026-05-14 13:48:33 -0600

    unit tests for BoundaryPoint

---
+++ Krystal.HTML.Tests/tests/Ranges/BoundaryPoint.hpp
@@ -0,0 +1,42 @@
+﻿#include "Krystal.HTML/Ranges/BoundaryPoint.hpp"
+#include "Krystal.HTML/Node/Document.hpp"
+#include "Krystal.HTML.Tests/TestContainerNode.hpp"
+#include <catch_all.hpp>
+
+namespace Krys::Tests
+{
+  using namespace Krys::HTML;
+
+  TEST_CASE("BoundaryPoint::ComparePositionTo", "[HTML][BoundaryPoint]")
+  {
+    auto document = CreateRef<Document>();
+    auto parent = CreateRef<TestContainerNode>(*document);
+    auto childA = CreateRef<TestContainerNode>(*document);
+    auto childB = CreateRef<TestContainerNode>(*document);
+
+    REQUIRE_FALSE(parent->AppendChild(*childA).HasException());
+    REQUIRE_FALSE(parent->AppendChild(*childB).HasException());
+
+    BoundaryPoint pointA {childA, 0};
+    BoundaryPoint pointB {childB, 0};
+
+    REQUIRE(pointA.ComparePositionTo(pointB) == std::strong_ordering::less);
+    REQUIRE(pointB.ComparePositionTo(pointA) == std::strong_ordering::greater);
+    REQUIRE(pointA.ComparePositionTo(pointA) == std::strong_ordering::equal);
+
+    BoundaryPoint pointAOffset {childA, 1};
+    BoundaryPoint pointBOffset {childB, 1};
+
+    REQUIRE(pointAOffset.ComparePositionTo(pointBOffset) == std::strong_ordering::less);
+    REQUIRE(pointBOffset.ComparePositionTo(pointAOffset) == std::strong_ordering::greater);
+    REQUIRE(pointAOffset.ComparePositionTo(pointAOffset) == std::strong_ordering::equal);
+
+
+    BoundaryPoint pointAParent {parent, 0};
+    BoundaryPoint pointBChild {childB, 0};
+
+    REQUIRE(pointAParent.ComparePositionTo(pointBChild) == std::strong_ordering::less);
+    REQUIRE(pointBChild.ComparePositionTo(pointAParent) == std::strong_ordering::greater);
+    REQUIRE(pointAParent.ComparePositionTo(pointAParent) == std::strong_ordering::equal);
+  }
+}
