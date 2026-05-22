158d2b92bfe2a8fc0e68b192dda18fd533e4328d
Dylan <dylanclarke890@gmail.com>
2026-05-21 22:25:15 -0600

    implemented some unit tests for

--- Krystal.HTML/include/Krystal.HTML/Node/DOMImplementation.hpp
+++ Krystal.HTML/include/Krystal.HTML/Node/DOMImplementation.hpp
@@ -13,6 +13,7 @@ namespace Krys::HTML
   class Document;
   class XMLDocument;
 
+  /// @see https://dom.spec.whatwg.org/#interface-domimplementation
   class DOMImplementation
   {
     friend class Document;
@@ -26,14 +27,23 @@ namespace Krys::HTML
     }
 
   public:
+    /// @brief Returns a doctype, with the given `name`, `publicId`, and `systemId`.
+    /// @throws InvalidCharacterError if `name` is not a valid doctype name.
     KRYS_NODISCARD ExceptionOr<Ref<DocumentType>> CreateDocumentType(const DOMString &name,
                                                                      const DOMString &publicId,
                                                                      const DOMString &systemId) noexcept;
 
+    /// @brief Returns an XMLDocument, with a document element whose local name is `qualifiedName` and whose
+    /// namespace is `namespaceUri` (unless `qualifiedName` is the empty string), and with `doctype`, if it is
+    /// given, as its doctype.
+    /// @throws This method throws the same exceptions as the createElementNS() method, when invoked with
+    /// `namespaceUri` and `qualifiedName`.
     KRYS_NODISCARD ExceptionOr<Ref<XMLDocument>>
       CreateDocument(DOMStringAtom namespaceUri, DOMStringAtom qualifiedName = DOMStringAtom::Null(),
                      RefPtr<DocumentType> &&doctype = nullptr) noexcept;
 
+    /// @brief Returns a document, with a basic tree already constructed including a title element, unless the
+    /// `title` is omitted.
     KRYS_NODISCARD ExceptionOr<Ref<Document>>
       CreateHTMLDocument(const Maybe<DOMString> &title = Null) noexcept;
 
