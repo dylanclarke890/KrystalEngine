8ca33543964c12e5aa896528da9ca5db9429319f
Dylan <dylanclarke890@gmail.com>
2026-05-24 18:20:22 -0600

    move some IDL files to match new folder structure, updated Document.idl with HTML spec extensions

---
+++ Krystal.HTML/include/Krystal.HTML/Node/Enums/DocumentReadyState.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "Krystal.Lib/Core/Enum.hpp"
+
+namespace Krys::HTML
+{
+  enum class DocumentReadyState : uint8
+  {
+    Loading,
+    Interactive,
+    Complete,
+  };
+}
+
+KRYS_DEFINE_CONTIGUOUS_ENUM_TRAITS(Krys::HTML::DocumentReadyState, 3uz);
---
+++ Krystal.HTML/include/Krystal.HTML/Node/Enums/DocumentVisibilityState.hpp
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "Krystal.Lib/Core/Enum.hpp"
+
+namespace Krys::HTML
+{
+  enum class DocumenVisibilityState : uint8
+  {
+    Visible,
+    Hidden,
+  };
+}
+
+KRYS_DEFINE_CONTIGUOUS_ENUM_TRAITS(Krys::HTML::DocumenVisibilityState, 2uz);
