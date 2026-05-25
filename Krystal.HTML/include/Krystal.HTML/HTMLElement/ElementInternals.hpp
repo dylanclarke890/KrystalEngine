b2e1d0a36c68bc7809d6f01ad70091d9efbc44e5
Dylan <dylanclarke890@gmail.com>
2026-05-25 01:43:01 -0600

    working on IDL Reflect

---
+++ Krystal.HTML/include/Krystal.HTML/HTMLElement/ElementInternals.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "Krystal.Lib/Mixins/RefCounted.hpp"
+#include "Krystal.Lib/Pointers/RefPtr.hpp"
+#include "Krystal.Lib/Pointers/WeakPtr.hpp"
+
+namespace Krys::HTML
+{
+  class HTMLElement;
+
+  class ElementInternals : public RefCounted<ElementInternals>
+  {
+    friend class ContentAttributeReflection;
+
+  private:
+    WeakPtr<HTMLElement> _target;
+
+  private:
+    KRYS_NODISCARD RawPtr<HTMLElement> TargetElement() noexcept
+    {
+      return _target.get();
+    }
+  };
+}
