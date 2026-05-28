572b35e871fabb20b91f4a6f0c34bc0fb6448fd4
Dylan <dylanclarke890@gmail.com>
2026-05-28 05:55:31 -0600

    further build optimisations, reduced build time by around 2 mins 20 seconds

---
+++ Krystal.HTML.Tests/pch.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+// Catch2 Testing Framework - Most expensive header (22s+ compile time)
+#include <catch_all.hpp>
+
+// Standard Library Headers - Expensive template-heavy headers
+#include <chrono>
+#include <format>
+#include <functional>
+#include <memory>
+#include <optional>
+#include <span>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <vector>
+
+// Commonly used Krystal.HTML headers
+#include "Krystal.HTML/DOM/Node.hpp"
+#include "Krystal.HTML/DOM/EventTarget.hpp"
+#include "Krystal.HTML/DOM/Document.hpp"
+#include "Krystal.HTML/Node/Element.hpp"
---
+++ Krystal.HTML/pch.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+// Standard Library Headers - Expensive template-heavy headers
+#include <algorithm>
+#include <chrono>
+#include <concepts>
+#include <format>
+#include <functional>
+#include <memory>
+#include <optional>
+#include <span>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <vector>
+
+// Krystal.Lib common headers
+#include "Krystal.Lib/Core/Concepts.hpp"
+#include "Krystal.Lib/String/String.hpp"
