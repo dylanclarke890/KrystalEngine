01e5a633068bbd8ab999a2d2d30cf124871b7f08
Dylan <dylanclarke890@gmail.com>
2026-05-28 22:13:18 -0600

    attr

--- Krystal.HTML/include/Krystal.HTML/DOM/Attr.hpp
+++ Krystal.HTML/include/Krystal.HTML/DOM/Attr.hpp
@@ -10,6 +10,7 @@ namespace Krys::HTML
   class Document;
   class Element;
 
+  /// @see https://dom.spec.whatwg.org/#interface-attr
   class Attr : public Node
   {
     KRYS_OVERRIDE_DELETE_FOR_CHECKED_PTR(Attr);
@@ -28,43 +29,51 @@ namespace Krys::HTML
     Attr(Document &document, const QualifiedName &qualifiedName, DOMString &&value = {}) noexcept;
 
   public:
-#pragma region Attr
+#pragma region Attr - https://dom.spec.whatwg.org/#attr
 
+    /// @see https://dom.spec.whatwg.org/#dom-attr-namespaceuri
     KRYS_NODISCARD DOMStringAtom NamespaceURI() const noexcept
     {
       return _name.NamespaceURI;
     }
 
+    /// @see https://dom.spec.whatwg.org/#dom-attr-prefix
     KRYS_NODISCARD DOMStringAtom Prefix() const noexcept
     {
       return _name.Prefix;
     }
 
+    /// @see https://dom.spec.whatwg.org/#dom-attr-localname
     KRYS_NODISCARD DOMStringAtom LocalName() const noexcept
     {
       return _name.LocalName;
     }
 
+    /// @see https://dom.spec.whatwg.org/#dom-attr-name
     KRYS_NODISCARD DOMString Name() const noexcept
     {
       return _name.Name();
     }
 
+    /// @see https://dom.spec.whatwg.org/#dom-attr-value
     KRYS_NODISCARD const DOMString &Value() const noexcept
     {
       return _value;
     }
 
+    /// @see https://dom.spec.whatwg.org/#dom-attr-value
     void Value(DOMString &&value) noexcept
     {
       SetExistingAttributeValue(*this, Krys::Move(value));
     }
 
+    /// @see https://dom.spec.whatwg.org/#dom-attr-ownerelement
     KRYS_NODISCARD RawPtr<Element> OwnerElement() const noexcept
     {
       return _ownerElement.get();
     }
 
+    /// @see https://dom.spec.whatwg.org/#dom-attr-specified
     KRYS_NODISCARD bool Specified() const noexcept
     {
       return true;
@@ -74,27 +83,32 @@ namespace Krys::HTML
 
 #pragma region Node
 
+    /// @see https://dom.spec.whatwg.org/#dom-node-nodename
     KRYS_NODISCARD DOMString NodeName() const noexcept final
     {
       return Name();
     }
 
+    /// @see https://dom.spec.whatwg.org/#dom-node-nodevalue
     KRYS_NODISCARD Maybe<DOMString> NodeValue() const noexcept final
     {
       return Value();
     }
 
+    /// @see https://dom.spec.whatwg.org/#dom-node-nodevalue
     ExceptionOr<void> NodeValue(DOMString &&value) noexcept final
     {
       SetExistingAttributeValue(*this, Krys::Move(value));
       return {};
     }
 
+    /// @see https://dom.spec.whatwg.org/#dom-node-textcontent
     KRYS_NODISCARD Maybe<DOMString> TextContent() const noexcept final
     {
       return Value();
     }
 
+    /// @see https://dom.spec.whatwg.org/#dom-node-textcontent
     ExceptionOr<void> TextContent(DOMString &&value) noexcept final
     {
       SetExistingAttributeValue(*this, Krys::Move(value));
