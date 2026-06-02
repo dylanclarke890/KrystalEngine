defc5f9c2627d1c82657ba2dc83faca299e3859d
Dylan <dylanclarke890@gmail.com>
2026-06-02 00:01:22 -0600

    basic idl reflection implementation for HTMLMetaElement

--- Krystal.HTML/include/Krystal.HTML/HTML/HTMLMetaElement.hpp
+++ Krystal.HTML/include/Krystal.HTML/HTML/HTMLMetaElement.hpp
@@ -12,7 +12,43 @@ namespace Krys::HTML
   public:
     HTMLMetaElement(Document &document) noexcept;
 
-    // TODO(impl): IDL attributes
+#pragma region HTMLMetaElement - https://html.spec.whatwg.org/#htmlmetaelement
+
+    /// @see https://html.spec.whatwg.org/#dom-meta-name
+    KRYS_NODISCARD DOMString Name() const noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-meta-name
+    void Name(DOMString &&value) noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-meta-httpequiv
+    KRYS_NODISCARD DOMString HttpEquiv() const noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-meta-httpequiv
+    void HttpEquiv(DOMString &&value) noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-meta-content
+    KRYS_NODISCARD DOMString Content() const noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-meta-content
+    void Content(DOMString &&value) noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-meta-media
+    KRYS_NODISCARD DOMString Media() const noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-meta-media
+    void Media(DOMString &&value) noexcept;
+
+#pragma endregion
+
+#pragma region HTMLMetaElement Obsolete members - https://html.spec.whatwg.org/#HTMLMetaElement-partial
+
+    /// @see https://html.spec.whatwg.org/#dom-meta-scheme
+    KRYS_NODISCARD DOMString Scheme() const noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-meta-scheme
+    void Scheme(DOMString &&value) noexcept;
+
+#pragma endregion
   };
 }
 
