6da14fb373a916907c22eec51395d7e9fd540a4f
Dylan <dylanclarke890@gmail.com>
2026-06-02 01:13:33 -0600

    HTMLBodyElement

--- Krystal.HTML/include/Krystal.HTML/HTML/HTMLBodyElement.hpp
+++ Krystal.HTML/include/Krystal.HTML/HTML/HTMLBodyElement.hpp
@@ -11,6 +11,46 @@ namespace Krys::HTML
 
   public:
     HTMLBodyElement(Document &document) noexcept;
+
+#pragma region HTMLBodyElement Obsolete members - https://html.spec.whatwg.org/#HTMLBodyElement-partial
+
+    /// @see https://html.spec.whatwg.org/#dom-body-text
+    KRYS_NODISCARD DOMString Text() const noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-body-text
+    void Text(DOMString &&value) noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-body-link
+    KRYS_NODISCARD DOMString Link() const noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-body-link
+    void Link(DOMString &&value) noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-body-vlink
+    KRYS_NODISCARD DOMString VLink() const noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-body-vlink
+    void VLink(DOMString &&value) noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-body-alink
+    KRYS_NODISCARD DOMString ALink() const noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-body-alink
+    void ALink(DOMString &&value) noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-body-bgcolor
+    KRYS_NODISCARD DOMString BgColor() const noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-body-bgcolor
+    void BgColor(DOMString &&value) noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-body-background
+    KRYS_NODISCARD DOMString Background() const noexcept;
+
+    /// @see https://html.spec.whatwg.org/#dom-body-background
+    void Background(DOMString &&value) noexcept;
+
+#pragma endregion
   };
 }
 
--- Krystal.HTML/src/HTML/HTMLBodyElement.cpp
+++ Krystal.HTML/src/HTML/HTMLBodyElement.cpp
@@ -2,6 +2,7 @@
 #include "Krystal.HTML/DOM/AbortSignal.hpp"
 #include "Krystal.HTML/DOM/Attr.hpp"
 #include "Krystal.HTML/DOM/ShadowRoot.hpp"
+#include "Krystal.HTML/HTML/Attributes/Reflection.hpp"
 #include "Krystal.HTML/HTML/CustomElement/CustomElementRegistry.hpp"
 #include "Krystal.HTML/HTML/HTMLSlotElement.hpp"
 
@@ -10,4 +11,68 @@ namespace Krys::HTML
   HTMLBodyElement::HTMLBodyElement(Document &document) noexcept : HTMLElement(document, HTMLTagName::Body)
   {
   }
+
+#pragma region HTMLBodyElement Obsolete members
+
+  DOMString HTMLBodyElement::Text() const noexcept
+  {
+    return Attributes::Reflection::Reflect<DOMString>(*this, u8"text").value_or(u8"");
+  }
+
+  void HTMLBodyElement::Text(DOMString &&value) noexcept
+  {
+    Attributes::Reflection::Reflect<DOMString>(*this, u8"text", Krys::Move(value));
+  }
+
+  DOMString HTMLBodyElement::Link() const noexcept
+  {
+    return Attributes::Reflection::Reflect<DOMString>(*this, u8"link").value_or(u8"");
+  }
+
+  void HTMLBodyElement::Link(DOMString &&value) noexcept
+  {
+    Attributes::Reflection::Reflect<DOMString>(*this, u8"link", Krys::Move(value));
+  }
+
+  DOMString HTMLBodyElement::VLink() const noexcept
+  {
+    return Attributes::Reflection::Reflect<DOMString>(*this, u8"vlink").value_or(u8"");
+  }
+
+  void HTMLBodyElement::VLink(DOMString &&value) noexcept
+  {
+    Attributes::Reflection::Reflect<DOMString>(*this, u8"vlink", Krys::Move(value));
+  }
+
+  DOMString HTMLBodyElement::ALink() const noexcept
+  {
+    return Attributes::Reflection::Reflect<DOMString>(*this, u8"alink").value_or(u8"");
+  }
+
+  void HTMLBodyElement::ALink(DOMString &&value) noexcept
+  {
+    Attributes::Reflection::Reflect<DOMString>(*this, u8"alink", Krys::Move(value));
+  }
+
+  DOMString HTMLBodyElement::BgColor() const noexcept
+  {
+    return Attributes::Reflection::Reflect<DOMString>(*this, u8"bgcolor").value_or(u8"");
+  }
+
+  void HTMLBodyElement::BgColor(DOMString &&value) noexcept
+  {
+    Attributes::Reflection::Reflect<DOMString>(*this, u8"bgcolor", Krys::Move(value));
+  }
+
+  DOMString HTMLBodyElement::Background() const noexcept
+  {
+    return Attributes::Reflection::Reflect<DOMString>(*this, u8"background").value_or(u8"");
+  }
+
+  void HTMLBodyElement::Background(DOMString &&value) noexcept
+  {
+    Attributes::Reflection::Reflect<DOMString>(*this, u8"background", Krys::Move(value));
+  }
+
+#pragma endregion
 }
