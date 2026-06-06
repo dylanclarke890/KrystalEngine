08ab287131b8b451f3e2ea968e010762ed95352c
Dylan <dylanclarke890@gmail.com>
2026-06-05 19:12:39 -0600

    fix: HTMLCanvasElement's type cast was against Node, not HTMLElement

--- Krystal.HTML/include/Krystal.HTML/HTML/HTMLCanvasElement.hpp
+++ Krystal.HTML/include/Krystal.HTML/HTML/HTMLCanvasElement.hpp
@@ -38,7 +38,7 @@ namespace Krys::HTML
 }
 
 KRYS_SPECIALIZE_TYPE_CAST_TRAITS_BEGIN(Krys::HTML::HTMLCanvasElement)
-  static bool IsType(const Krys::HTML::Node &target) noexcept
+  static bool IsType(const Krys::HTML::HTMLElement &target) noexcept
   {
     return target.IsHTMLCanvasElement();
   }
