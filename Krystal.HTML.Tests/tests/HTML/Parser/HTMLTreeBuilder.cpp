82c39870737002b2094326ff7b185622abfd356c
Dylan <dylanclarke890@gmail.com>
2026-07-03 02:51:07 -0600

    delete HTMLTreeBuui

--- Krystal.HTML.Tests/tests/HTML/Parser/HTMLTreeBuilder.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include "Krystal.HTML/HTML/Parser/HTMLTreeBuilder.hpp"
-#include "Krystal.HTML.Tests/ParserTestUtils.hpp"
-#include <catch_all.hpp>
-
-namespace Krys::Tests
-{
-  using namespace Krys::HTML;
-}
