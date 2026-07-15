67be4b63d7ec8aad8bcae8074088a1e7df6d6429
Dylan <dylanclarke890@gmail.com>
2026-07-15 16:57:32 -0600

    add typedef for CSSOMString

---
+++ Krystal.HTML/include/Krystal.HTML/CSS/Types/CSSOMString.hpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include "Krystal.HTML/DOM/Types/DOMString.hpp"
+
+namespace Krys::HTML
+{
+  /// @see https://www.w3.org/TR/cssom-1/#cssomstring-type
+  using CSSOMString = DOMString;
+}
