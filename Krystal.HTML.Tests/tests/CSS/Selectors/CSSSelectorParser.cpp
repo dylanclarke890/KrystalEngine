e643398a161f45eb23a4aaa58d74785e2a21e19f
Dylan <dylanclarke890@gmail.com>
2026-07-25 06:01:20 -0600

    set up basic test. fails because obviously.

---
+++ Krystal.HTML.Tests/tests/CSS/Selectors/CSSSelectorParser.cpp
@@ -0,0 +1,18 @@
+﻿#include "Krystal.HTML/CSS/Selectors/CSSSelectorParser.hpp"
+#include "Krystal.HTML/CSS/CSSImportRule.hpp"
+#include "Krystal.HTML/CSS/CSSNamespaceRule.hpp"
+#include "Krystal.HTML/CSS/CSSRuleList.hpp"
+#include "Krystal.HTML/CSS/CSSStyleRule.hpp"
+#include "Krystal.HTML/CSS/CSSStyleSheet.hpp"
+#include "Krystal.HTML/CSS/MediaList.hpp"
+#include "Krystal.HTML/CSS/StyleSheetContents.hpp"
+#include <catch_all.hpp>
+
+namespace Krys::HTML::Tests
+{
+  TEST_CASE("CSSSelectorParser", "[CSS][Selectors]")
+  {
+    auto result = CSSSelectorParser::ParseSelector(u8"div", {}, nullptr, Null);
+    REQUIRE(result);
+  }
+}
