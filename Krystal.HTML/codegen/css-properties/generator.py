edb6f3b3031ddd93fd77e9043264874b1e0c58ec
Dylan <dylanclarke890@gmail.com>
2026-08-07 20:10:37 -0600

    generator now generates body of MatchingShorthandsForLonghand function

--- Krystal.HTML/codegen/css-properties/generator.py
+++ Krystal.HTML/codegen/css-properties/generator.py
@@ -32,7 +32,7 @@ class CSSProperty:
         return EnumValue(self.id, self.id_pascal_case, comment)
 
 
-def __generate_css_property_enum(
+def __generate_css_property_id_enum(
     context: GeneratorContext,
     all_properties: list[CSSProperty],
     shorthand_property_count: int,
@@ -67,53 +67,89 @@ class ShorthandToLonghandData:
         self.longhand_names = longhands
 
 
-def __generate_shorthand_to_longhand_mapping(
+class LonghandsToShorthandData:
+    longhand_names: list[str]
+    shorthand_names: list[str]
+
+    def __init__(self, longhand: list[str], shorthands: list[str]):
+        self.longhand_names = longhand
+        self.shorthand_names = shorthands
+
+
+def __generate_css_property_shorthand_files(
     context: GeneratorContext, shorthand_properties: list[CSSProperty], longhand_properties: list[CSSProperty]
 ):
-    shorthand_to_longhand_data: list[ShorthandToLonghandData] = []
-    for prop in shorthand_properties:
-        if prop.id == "all":
-            longhand_names = [lh.id_pascal_case for lh in longhand_properties]
-        else:
-            longhand_names = [kebab_case_to_pascal_case(lh) for lh in prop.longhand_names]  # type: ignore
-        shorthand_to_longhand_data.append(ShorthandToLonghandData(prop.id_pascal_case, longhand_names))
+    model: dict[str, object] = generator_metadata(GENERATOR_NAME)
 
-    model: dict[str, object] = {
-        "shorthand_to_longhand_data": shorthand_to_longhand_data,
-    }
-    model.update(generator_metadata(GENERATOR_NAME))
+    shorthand_to_longhand_data: dict[str, list[CSSProperty]] = {}
+    longhand_to_shorthand_data: dict[str, list[CSSProperty]] = {}
+    shorthand_to_longhand_count: dict[CSSProperty, int] = {}
+    longhand_properties_by_name: dict[str, CSSProperty] = {prop.id: prop for prop in longhand_properties}
+
+    for property in shorthand_properties:
+        shorthand_to_longhand_count[property] = 0
+        for longhand_name in property.longhand_names or []:
+            if longhand_name == "all":
+                for inner_property in longhand_properties:
+                    if inner_property.id == "direction" or inner_property.id == "unicode-bidi":
+                        continue
+                    longhand_to_shorthand_data.setdefault(inner_property.id_pascal_case, []).append(property)
+                    shorthand_to_longhand_count[property] += 1
+            else:
+                id = longhand_properties_by_name[longhand_name].id_pascal_case
+                longhand_to_shorthand_data.setdefault(id, []).append(property)
+                shorthand_to_longhand_count[property] += 1
+
+        if property.id == "all":
+            shorthand_to_longhand_data[property.id_pascal_case] = [
+                inner_property
+                for inner_property in longhand_properties
+                if inner_property.id != "direction" and inner_property.id != "unicode-bidi"
+            ]
+        else:
+            shorthand_to_longhand_data[property.id_pascal_case] = [
+                longhand_properties_by_name[name] for name in property.longhand_names or []
+            ]
+
+    # https://drafts.csswg.org/cssom/#concept-shorthands-preferred-order
+    def preferred_order_for_shorthands(x):
+        return (-shorthand_to_longhand_count[x], x.id.startswith("-"), not x.id.startswith("-webkit-"), x.id)
+
+    model.update(
+        {
+            "shorthand_to_longhand_data": [
+                ShorthandToLonghandData(shorthand, [longhand.id_pascal_case for longhand in longhands])
+                for shorthand, longhands in shorthand_to_longhand_data.items()
+            ],
+            "longhands_to_shorthand_data": [
+                LonghandsToShorthandData([longhand], [shorthand.id_pascal_case for shorthand in sorted(shorthands, key=preferred_order_for_shorthands)])
+                for longhand, shorthands in longhand_to_shorthand_data.items()
+            ],
+            "shorthand_to_longhand_count": shorthand_to_longhand_count,
+        }
+    )
 
     template = context.template_env.get_template("css-property-shorthand.cpp.jinja")
     output = template.render(model=model)
 
-    output_dir = context.project_root / "src" / "CSS" / "Properties"
-    with open(output_dir / f"CSSPropertyShorthand.cpp", "w") as cpp_file:
+    output_filepath = context.project_root / "src" / "CSS" / "Properties" / "CSSPropertyShorthand.cpp"
+    with open(output_filepath, "w") as cpp_file:
         cpp_file.write(output)
 
 
-class LonghandToShorthandData:
-    longhand_name: str
-    shorthand_names: list[str]
-
-    def __init__(self, longhand: str, shorthands: list[str]):
-        self.longhand_name = longhand
-        self.shorthand_names = shorthands
-
-
 def generate(context: GeneratorContext):
-    properties_json = get_validated_json( pathlib.Path(__file__).parent)
+    properties_json = get_validated_json(pathlib.Path(__file__).parent)
 
     shorthand_properties: list[CSSProperty] = []
     longhand_properties: list[CSSProperty] = []
     for property_name, property_data in properties_json["properties"].items():
         css_property = CSSProperty.from_json_dict(property_name, property_data)
         if css_property.longhand_names is not None:
-            # If the property has longhands, it is a shorthand property.
             shorthand_properties.append(css_property)
         else:
             longhand_properties.append(css_property)
     all_properties = shorthand_properties + longhand_properties
 
-    __generate_css_property_enum(context, all_properties, len(shorthand_properties))
+    __generate_css_property_id_enum(context, all_properties, len(shorthand_properties))
 
-    __generate_shorthand_to_longhand_mapping(context, shorthand_properties, longhand_properties)
+    __generate_css_property_shorthand_files(context, shorthand_properties, longhand_properties)
